923de0d58dbabf408c028395167ac5589c7cba22
brajeshkumar60730 <139232573+brajeshkumar60730@users.noreply.github.com>
2024-06-05 18:18:15 +0530

    first commit

---
+++ src/app/models/products.model.ts
@@ -0,0 +1,12 @@
+export class Products {
+        productId: any;
+        productName?: string;
+        productCost?: number;
+        productDescription?: string;
+        productStock?: number;
+        gender?: string;
+        country?: string; 
+        firstDate?: Date;
+        lastDate?: Date;
+        
+}
